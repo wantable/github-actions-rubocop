0123212a57745babe14c4da05dba7a65f0d2fda1
Luiz Fonseca <eu@luiz.cc>
2020-01-09 15:59:00 +0100

    Update gems
    Update Rubocop gem versions

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-gem install rubocop -v 0.68.1
-gem install rubocop-performance -v 1.3.0
+gem install rubocop -v 0.79.0
+gem install rubocop-performance -v 1.5.2
 gem install rubocop-rails -v 2.4.1
 
 ruby /action/lib/index.rb
