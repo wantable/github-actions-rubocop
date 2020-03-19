1e9639a68d5c434a268c406b068cba9540992fba
Luiz Fonseca <eu@luiz.cc>
2020-03-19 23:21:35 +0100

    Update entrypoint.sh

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-gem install rubocop -v 0.79.0
+gem install rubocop -v 0.80.1
 gem install rubocop-performance -v 1.5.2
-gem install rubocop-rails -v 2.4.1
+gem install rubocop-rails -v 2.4.2
 
 ruby /action/lib/index.rb
