1ac40475351ef4fb71785e0867fe7c189fa85f73
Nazar Vinnychuk <ux.isco@gmail.com>
2021-06-24 17:49:46 +0300

    Update rubocop and add rubocop-minitest

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-gem install rubocop -v 1.11
-gem install rubocop-performance -v 1.10
-gem install rubocop-rails -v 2.9
+gem install rubocop -v 1.17
+gem install rubocop-minitest -v 0.13
+gem install rubocop-performance -v 1.11
+gem install rubocop-rails -v 2.11
 
 ruby /action/lib/index.rb
