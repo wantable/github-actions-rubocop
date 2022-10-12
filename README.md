9f515def13b3cc2e96a85f15917cbca0b26a544d
Casey Lopez <casey@sobrilsky.com>
2022-10-12 15:44:45 -0500

    rubocop-1.31

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ Lint your Ruby code in parallel to your builds.
 
 ## How it works
 
-- Ruby 2.6.5
-- Rubocop + Rubocop Performance
+- Ruby 3.1.2
+- Rubocop + Rubocop Performance + Rubocop Rails
 
 
 ![](screenshots/annotations.png)
--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-gem install rubocop -v 0.80.1
-gem install rubocop-rspec -v 1.38.1
-gem install rubocop-performance -v 1.5.2
-gem install rubocop-rails -v 2.4.2
+gem install rubocop -v 1.31
+gem install rubocop-minitest -v 0.13
+gem install rubocop-performance -v 1.11
+gem install rubocop-rails -v 2.11
 
 ruby /action/lib/index.rb
