6a1257a9d314c673c56b18a117c9ff705f8c71df
Kevin Solkowski <ksolkowski@gmail.com>
2024-02-23 12:38:13 -0600

    maybe new ruby version too?

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-gem install haml_lint -v 0.53.0
+gem install haml_lint -v 0.57.0
 gem install rubocop -v 1.22.3
+gem install rubocop-minitest -v 0.13.0
 gem install rubocop-performance -v 1.11.5
 gem install rubocop-rails -v 2.11.3
 
