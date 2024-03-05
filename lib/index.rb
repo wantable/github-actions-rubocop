f982e4e00ae0928f26f57982fe280dccfbf17d5e
Kevin Solkowski <ksolkowski@gmail.com>
2024-03-04 18:02:01 -0600

    Update index.rb

--- lib/index.rb
+++ lib/index.rb
@@ -56,11 +56,9 @@ def update_check(id, conclusion, output)
   http = Net::HTTP.new('api.github.com', 443)
   http.use_ssl = true
   path = "/repos/#{@owner}/#{@repo}/check-runs/#{id}"
-  puts path.inspect
+  puts body.inspect
   resp = http.patch(path, body.to_json, @headers)
   parsed = JSON.parse(resp.body)
-  puts parsed.keys.inspect
-  puts parsed['output']['annotations_count'].inspect
   raise resp.message if resp.code.to_i >= 300
 end
 
