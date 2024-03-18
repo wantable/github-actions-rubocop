ee6d615a1af2b5b13141db18e176eb677d7528e7
Abid Mahmood <mabid.mahmood@gmail.com>
2024-03-18 23:55:20 +0500

    rubocop 1.62.1

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-gem install rubocop -v 1.22.3
+gem install rubocop -v 1.62.1
 gem install rubocop-minitest -v 0.13
 gem install rubocop-performance -v 1.11.5
-gem install rubocop-rails -v 2.11.3
+gem install rubocop-rails -v 2.24.0
 
 ruby /action/lib/index.rb
