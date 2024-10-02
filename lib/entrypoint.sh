7ce743f2a1a8dd349ee9f7d6b3f84c7989ad6b75
Nick Bucciarelli <nick.bucciarelli@wantable.com>
2024-10-02 12:44:27 -0400

    Moved back

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -17,13 +17,10 @@ echo "MULTI_LINES_TEXT_ENV_VAR<<EOF" >> $GITHUB_OUTPUT
 
 # here we can place the command that will generate multi-line text
 echo $(haml-lint -r github --no-summary | sed "s/\`//g" | sed "s/'//g" | sed "s/\"//g") >> $GITHUB_OUTPUT
-
-echo "EOF" >> $GITHUB_OUTPUT
-
-
-echo "CSS_LINT_VAR<<EOF" >> $GITHUB_OUTPUT
 echo $(scss-lint | sed “s/\`//g” | sed “s/‘//g” | sed “s/\“//g”) >> $GITHUB_OUTPUT
+
 echo "EOF" >> $GITHUB_OUTPUT
+
 
 
 # export IFS="\n"
