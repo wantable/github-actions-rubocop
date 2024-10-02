229c0eac6b5f2f9a5c52564d6795be095816d4b5
Nick Bucciarelli <nick.bucciarelli@wantable.com>
2024-10-02 14:57:03 -0400

    Oops

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 gem install haml_lint -v 0.57.0
 
-gem install scss_lint -v 60.0
+gem install scss_lint -v 0.60.0
 gem install rubocop -v 1.62
 gem install rubocop-minitest -v 0.13.0
 gem install rubocop-performance -v 1.11.5
