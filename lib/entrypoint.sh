2e1161a49196bbf681dae051ed9965d8a87e3999
Nick Bucciarelli <nick.bucciarelli@wantable.com>
2024-10-02 11:54:32 -0400

    Here we go?

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -3,6 +3,7 @@
 set -e
 
 gem install haml_lint -v 0.57.0
+gem install scss_lint -v 0.60.0
 gem install rubocop -v 1.62
 gem install rubocop-minitest -v 0.13.0
 gem install rubocop-performance -v 1.11.5
@@ -11,12 +12,14 @@ gem install rubocop-rails -v 2.24.0
 #echo $(haml-lint app/views/w3/users/ -r github --no-summary)
 # echo "RESULT=$(haml-lint app/views/w3/users/ -r github --no-summary)" >> $GITHUB_OUTPUT
 
- echo "MULTI_LINES_TEXT_ENV_VAR<<EOF" >> $GITHUB_OUTPUT
+echo "MULTI_LINES_TEXT_ENV_VAR<<EOF" >> $GITHUB_OUTPUT
+# echo "MULTI_LINES_TEXT_ENV_VAR<<EOF" >> $GITHUB_OUTPUT
 
- # here we can place the command that will generate multi-line text
- echo $(haml-lint -r github --no-summary | sed "s/\`//g" | sed "s/'//g" | sed "s/\"//g") >> $GITHUB_OUTPUT
+# here we can place the command that will generate multi-line text
+echo $(haml-lint -r github --no-summary | sed "s/\`//g" | sed "s/'//g" | sed "s/\"//g") >> $GITHUB_OUTPUT
+echo $(scss-lint -r github --no-summary | sed "s/\`//g" | sed "s/'//g" | sed "s/\"//g") >> $GITHUB_OUTPUT
 
- echo "EOF" >> $GITHUB_OUTPUT
+echo "EOF" >> $GITHUB_OUTPUT
 
 # export IFS="\n"
 # sentence=$(haml-lint app/views/w3/users/ -r github --no-summary)
